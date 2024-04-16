7426f05f3a8387973e27c6cca5d6d7d2585edafb
Jamie <77027304+itisjamied@users.noreply.github.com>
2024-04-16 13:37:25 -0400

    viewport based animations

--- scripts/script.js
+++ scripts/script.js
@@ -1,28 +1,39 @@
+function isInViewport(element) {
+  const rect = element.getBoundingClientRect();
+  return (
+    rect.bottom > 0 && // bottom must be below the top of the viewport
+    rect.right > 0 && // right side must be right of the viewport's left edge
+    rect.top < (window.innerHeight || document.documentElement.clientHeight) && // top must be above the bottom of the viewport
+    rect.left < (window.innerWidth || document.documentElement.clientWidth) // left must be left of the viewport's right edge
+  );
+}
+
 document.addEventListener("DOMContentLoaded", function () {
-  // add initial fadeIn effects with delays
   const fadeInUpElements = document.querySelectorAll(".fadeInUp:not(nav)");
   fadeInUpElements.forEach(function (element, index) {
     element.style.animationDelay = `${index * 600}ms`;
   });
 
-  // handle outgoing navigation
   document.querySelectorAll("a").forEach((link) => {
     link.addEventListener("click", function (e) {
-      // prevent default link behavior
-      e.preventDefault();
+      e.preventDefault(); // stop the link from navigating immediately
       const targetUrl = this.href;
 
-      // apply fadeOutDown in reverse order
-      [...fadeInUpElements].reverse().forEach((element, index) => {
+      let delayCounter = 0; // counter to track of the number of visible elements
+      const visibleElements = [...fadeInUpElements]
+        .filter(isInViewport)
+        .reverse();
+
+      visibleElements.forEach((element, index) => {
         element.classList.remove("fadeInUp");
         element.classList.add("fadeOutDown");
         element.style.animationDelay = `${index * 600}ms`;
+        delayCounter++; // increment for each visible element
       });
 
-      // delay navigation to allow animation to complete
       setTimeout(() => {
         window.location.href = targetUrl;
-      }, fadeInUpElements.length * 600 + 500); // 500 additional delay to ensure completion
+      }, delayCounter * 600 + 500); // adjust navigation delay based on visible elements
     });
   });
 });
