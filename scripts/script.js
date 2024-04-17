107cab4fe99ea0bf2efa70bd4979dea4e2f6731e
Jamie <77027304+itisjamied@users.noreply.github.com>
2024-04-17 13:57:15 -0400

    footer fade in and out

--- scripts/script.js
+++ scripts/script.js
@@ -1,61 +1,69 @@
 function isInViewport(element) {
   const rect = element.getBoundingClientRect();
   return (
-    rect.bottom > 0 && // bottom must be below the top of the viewport
-    rect.right > 0 && // right side must be right of the viewport's left edge
-    rect.top < (window.innerHeight || document.documentElement.clientHeight) && // top must be above the bottom of the viewport
-    rect.left < (window.innerWidth || document.documentElement.clientWidth) // left must be left of the viewport's right edge
+    rect.bottom > 0 &&
+    rect.right > 0 &&
+    rect.top < (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.left < (window.innerWidth || document.documentElement.clientWidth)
   );
 }
 
-document.addEventListener("DOMContentLoaded", function () {
-  const fadeInUpElements = document.querySelectorAll(".fadeInUp:not(nav)");
-  fadeInUpElements.forEach(function (element, index) {
+function handleNavigation(fadeInUpElements) {
+  const navLinks = document.querySelectorAll("nav a");
+  const logoLinks = document.querySelectorAll("header a img");
+  const footerLinks = document.querySelectorAll("footer a img");
+
+  [...navLinks, ...logoLinks, ...footerLinks].forEach((img) => {
+    const anchor = img.closest("a"); // This finds the nearest ancestor which is an anchor
+    if (!anchor) return;
+    anchor.addEventListener("click", (e) => {
+      e.preventDefault();
+      const targetUrl = anchor.href; // Use the href from the anchor, not the img
+      let delayCounter = 0;
+
+      fadeInUpElements
+        .filter(isInViewport)
+        .reverse()
+        .forEach((element, index) => {
+          element.classList.replace("fadeInUp", "fadeOutDown");
+          element.style.animationDelay = `${index * 600}ms`;
+          delayCounter++;
+        });
+
+      setTimeout(
+        () => (window.location.href = targetUrl),
+        delayCounter * 600 + 500
+      );
+    });
+  });
+}
+
+function animateOnLoad() {
+  const fadeInUpElements = Array.from(
+    document.querySelectorAll(".fadeInUp:not(nav)")
+  );
+  fadeInUpElements.forEach((element, index) => {
     element.style.animationDelay = `${index * 600}ms`;
   });
 
-  document.querySelectorAll("a").forEach((link) => {
-    link.addEventListener("click", function (e) {
-      e.preventDefault(); // stop the link from navigating immediately
-      const targetUrl = this.href;
+  handleNavigation(fadeInUpElements);
 
-      let delayCounter = 0; // counter to track of the number of visible elements
-      const visibleElements = [...fadeInUpElements]
-        .filter(isInViewport)
-        .reverse();
-
-      visibleElements.forEach((element, index) => {
-        element.classList.remove("fadeInUp");
-        element.classList.add("fadeOutDown");
-        element.style.animationDelay = `${index * 600}ms`;
-        delayCounter++; // increment for each visible element
-      });
-
-      setTimeout(() => {
-        window.location.href = targetUrl;
-      }, delayCounter * 600 + 500); // adjust navigation delay based on visible elements
-    });
-  });
-
-  setTimeout(function () {
+  setTimeout(() => {
     const player = document.getElementById("lottieAnimation");
-    player.play();
-  }, 1200); // Delay in milliseconds (5000ms = 5 seconds)
-});
-
-document.addEventListener("DOMContentLoaded", function () {
-  animateOncePerSession("animatedHeader", "animated-header");
-  animateOncePerSession("animatedNav", "animated-nav");
-  animateOncePerSession("animatedFooter", "animated-footer");
-});
+    if (player) player.play();
+  }, 2000);
+}
 
 function animateOncePerSession(elementId, animationClass) {
-  var element = document.getElementById(elementId);
-  if (element && !sessionStorage.getItem(elementId + "Animated")) {
-    // Apply the specified animation class
+  const element = document.getElementById(elementId);
+  if (element && !sessionStorage.getItem(`${elementId}Animated`)) {
     element.classList.add(animationClass);
-
-    // Set the flag in session storage
-    sessionStorage.setItem(elementId + "Animated", "true");
+    sessionStorage.setItem(`${elementId}Animated`, "true");
   }
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+  animateOnLoad();
+  animateOncePerSession("animatedHeader", "animated-header");
+  animateOncePerSession("animatedNav", "animated-nav");
+});
