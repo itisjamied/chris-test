c7d002ea78345f945ee9e5657c12fd6a4d6aa6a8
Jamie <77027304+itisjamied@users.noreply.github.com>
2024-04-30 11:11:05 -0400

    loose service worker caching logic

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,12 @@
-const CACHE_NAME = 'v1';
-const urlsToCache = [
-  'images/logo.svg',
-  'css/style.css',
-  'scripts/script.js',
-  'index.html',
-  '/pages/our-approach.html',
-  '/pages/leadership.html'
-];
+// const CACHE_NAME = 'v1';
+// const urlsToCache = [
+//   'images/logo.svg',
+//   'css/style.css',
+//   'scripts/script.js',
+//   'index.html',
+//   '/pages/our-approach.html',
+//   '/pages/leadership.html'
+// ];
 
 // self.addEventListener('install', event => {
 //   // Perform install steps
@@ -40,57 +40,53 @@ const urlsToCache = [
 //   );
 // });
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(urlsToCache);
-      })
-      .then(() => self.skipWaiting())
-      .then(() => {
-        if (navigator.storage && navigator.storage.persist)
-          return navigator.storage.persist();
-      })
-      .then(persistent => {
-        if (persistent)
-          console.log("The storage will be persistent");
-        else
-          console.log("The storage may not be persistent");
-      })
-  );
-});
+// self.addEventListener("install", (event) => {
+//   event.waitUntil(
+//     caches
+//       .open(CACHE_NAME)
+//       .then((cache) => {
+//         return cache.addAll(urlsToCache);
+//       })
+//       .then(() => self.skipWaiting())
+//       .then(() => {
+//         if (navigator.storage && navigator.storage.persist)
+//           return navigator.storage.persist();
+//       })
+//       .then((persistent) => {
+//         if (persistent) console.log("The storage will be persistent");
+//         else console.log("The storage may not be persistent");
+//       })
+//   );
+// });
 
-  
-  self.addEventListener('activate', event => {
-    event.waitUntil(self.clients.claim()); // claims clients immediately for the active worker to take control
-  });
-  
-  // self.addEventListener('fetch', event => {
-  //   event.respondWith(
-  //     caches.match(event.request).then(cachedResponse => {
-  //       return cachedResponse || fetch(event.request);
-  //     })
-  //   );
-  // });
-  
-  self.addEventListener('fetch', event => {
-    event.respondWith(
-      caches.match(event.request)
-        .then(cachedResponse => {
-          if (cachedResponse) {
-            console.log(`Cache hit: ${event.request.url}`);
-            return cachedResponse;
-          }
-  
-          console.log(`Cache miss: ${event.request.url}. Fetching from network...`);
-          return fetch(event.request).then(response => {
-            // Put the new resource into the cache
-            return caches.open(CACHE_NAME).then(cache => {
-              cache.put(event.request, response.clone());
-              return response;
-            });
-          });
-        })
-    );
-  });
-  
+// self.addEventListener("activate", (event) => {
+//   event.waitUntil(self.clients.claim()); // claims clients immediately for the active worker to take control
+// });
+
+// self.addEventListener('fetch', event => {
+//   event.respondWith(
+//     caches.match(event.request).then(cachedResponse => {
+//       return cachedResponse || fetch(event.request);
+//     })
+//   );
+// });
+
+// self.addEventListener("fetch", (event) => {
+//   event.respondWith(
+//     caches.match(event.request).then((cachedResponse) => {
+//       if (cachedResponse) {
+//         console.log(`Cache hit: ${event.request.url}`);
+//         return cachedResponse;
+//       }
+
+//       console.log(`Cache miss: ${event.request.url}. Fetching from network...`);
+//       return fetch(event.request).then((response) => {
+//         // Put the new resource into the cache
+//         return caches.open(CACHE_NAME).then((cache) => {
+//           cache.put(event.request, response.clone());
+//           return response;
+//         });
+//       });
+//     })
+//   );
+// });
