30061ffbe12d0fc9f8ae5919d6b0eddd9c3d4162
Jamie <77027304+itisjamied@users.noreply.github.com>
2024-05-30 11:31:39 -0400

    Full staff block updated via javascript

--- scripts/data.js
+++ scripts/data.js
@@ -4,7 +4,43 @@ function updateContent() {
     tagline: {
       home: "Generating business outcomes through a human-centered approach.",
       approach: "We're Different",
+      approachHead: "A. Our Approach.",
       leadership: "Battle Tested",
+      leadershipHead: "B. Our Leadership.",
+    },
+    team: {
+      member01: {
+        name: "Christopher Pross",
+        title: "Managing Partner",
+        imgSrc: "../images/lipp-pross-photo.png",
+        bio: `Christopher is an accomplished product and design leader, recognized for his track record guiding teams at both agencies and large incumbents. His expertise has been pivotal in tackling complex business challenges, collaborating with the likes of algorithmic trading firms, governments, healthcare advocacy groups, and CPG brands. His broad experience fuels his ability to draw unique parallels across industries, consistently generating outsized outcomes. His contributions have resulted in innovative digital products, new business models, and the strategic redesign of organizations. <a href="../images/launch-letter.pdf" target="_blank">Read why Chris founded ValueForm.</a>`,
+        clients: {
+          category01: {
+            name: "Finance",
+            list: "Goldman Sachs, Jane Street, MetLife, BNY Mellon",
+          },
+          category02: {
+            name: "Government",
+            list: "U.S. Dept. of Education, City of Boston, City of Philadelphia, City of Pittsburgh",
+          },
+          category03: {
+            name: "Other",
+            list: "Southwest Airlines, Radisson Hotels, Chick-fil-A, Brooks Running, Tabasco",
+          },
+        },
+        contact: {
+          method01: {
+            name: "Email:",
+            link: "malito:c.pross@valueform.io",
+            display: "c.pross@valueform.io",
+          },
+          method02: {
+            name: "LinkedIn:",
+            link: "https://www.linkedin.com/in/christopher-pross-jr-a7362244/",
+            display: "christopher-pross-jr",
+          },
+        },
+      },
     },
   };
 
@@ -18,8 +54,23 @@ function updateContent() {
     const key = element.getAttribute("key");
     const value = getNestedValue(textData, key);
     if (value) {
-      element.textContent = value;
+      if (element.tagName.toLowerCase() === "img") {
+        element.src = value;
+      } else if (
+        element.tagName.toLowerCase() === "a" &&
+        element.hasAttribute("data-display-key")
+      ) {
+        element.href = value;
+        const displayKey = element.getAttribute("data-display-key");
+        const displayValue = getNestedValue(textData, displayKey);
+        if (displayValue) {
+          element.textContent = displayValue;
+        }
+      } else {
+        element.innerHTML = value;
+      }
     }
   });
 }
+
 window.onload = updateContent;
