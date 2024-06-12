f5f348a6db344b4fc8264063899f2f252d780681
Jamie <77027304+itisjamied@users.noreply.github.com>
2024-06-12 11:29:47 -0400

    Pulling img src as string

--- scripts/fetchData.js
+++ scripts/fetchData.js
@@ -1,4 +1,5 @@
 const { createClient } = require("@sanity/client");
+const imageUrlBuilder = require("@sanity/image-url");
 
 const client = createClient({
   projectId: "wfwxz1rq", // your project ID
@@ -7,13 +8,24 @@ const client = createClient({
   useCdn: false, // `false` if you want to ensure fresh data
 });
 
+const builder = imageUrlBuilder(client);
+
+function urlFor(source) {
+  return builder.image(source).url();
+}
+
 // Query to fetch team members
 const query = '*[_type == "teamMember"] {title, name, bio, imgSrc}';
 
 client
   .fetch(query)
   .then((teamMembers) => {
-    console.log("Team Members:", teamMembers);
+    const teamMembersWithImageUrls = teamMembers.map((member) => ({
+      ...member,
+      imgSrc: member.imgSrc ? urlFor(member.imgSrc) : null,
+    }));
+
+    console.log("Team Members:", teamMembersWithImageUrls);
   })
   .catch((err) => {
     console.error("Error fetching team members:", err);
